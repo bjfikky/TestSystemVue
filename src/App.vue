bb7b9e47ff6f5307304fa7d83de893a0f21bc638
Benjamin Orinoloye <bjfikky@yahoo.com>
2019-02-14 23:06:57 -0500

    Added routing

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
 <template>
-	<div id="app">
-		<NavBar msg="Welcome to Your Vue.js App"/>
-    <Footer/>
-	</div>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
-
-<script>
-import NavBar from './components/NavBar.vue'
-import Footer from './components/Footer.vue'
-
-export default {
-  name: 'app',
-  components: {
-    NavBar, Footer
-  }
-}
-</script>
 
 <style>
 #app {
