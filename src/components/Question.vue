86d6174efa4efa26f61fa574782f1364389d803b
Benjamin Orinoloye <bjfikky@yahoo.com>
2019-02-15 00:03:41 -0500

    Addded Question component

---
+++ src/components/Question.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod ?</h5>
+            <hr/>
+            <Options/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Options from '@/components/Options'
+
+    export default {
+        name: 'question',
+        components: {
+            Options
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
